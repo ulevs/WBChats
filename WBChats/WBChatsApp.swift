0e00c645afc4b230bb7f7d2acbf7df0a7043e16d
uliana <ulianaevstigneeva@yandex.ru>
2024-06-12 16:39:20 +0300

    Initial Commit

---
+++ WBChats/WBChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBChatsApp.swift
+//  WBChats
+//
+//  Created by Ульяна Евстигнеева on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
